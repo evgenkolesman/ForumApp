9e97a51f2ad45d498bc9f6a097174efe33791741
evgenkolesman <glavstroi_e@mail.ru>
2021-09-30 20:36:20 +0300

    1. Spring boot repository [#2095] переделал на service и убрал коммиты

--- src/main/java/forum/controller/IndexController.java
+++ src/main/java/forum/controller/IndexController.java
@@ -1,7 +1,6 @@
 package forum.controller;
 
 import forum.repository.PostRepository;
-import forum.service.CommonService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
