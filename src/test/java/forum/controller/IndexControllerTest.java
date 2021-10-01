4c485752f4fc03d4bb4f5835284bd1bcbaf725e7
evgenkolesman <glavstroi_e@mail.ru>
2021-10-01 13:11:39 +0300

    0. Spring test [#6881] + подключаем h2 для тестирования

--- src/test/java/forum/controller/IndexControllerTest.java
+++ src/test/java/forum/controller/IndexControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
+@PropertySource(value="test.properties")
 class IndexControllerTest {
 
     @Autowired
