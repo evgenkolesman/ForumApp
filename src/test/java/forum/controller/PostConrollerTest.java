fe2e3f2cb518a63365b78586aa1032b9f1f0175b
evgenkolesman <glavstroi_e@mail.ru>
2021-10-03 13:30:04 +0300

    1. Spring mock [#2096] тесты не заходят

--- src/test/java/forum/controller/PostConrollerTest.java
+++ src/test/java/forum/controller/PostConrollerTest.java
@@ -4,10 +4,9 @@ import forum.Main;
 import forum.model.Post;
 import forum.repository.PostRepository;
 import forum.service.CommonService;
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,17 +14,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Calendar;
-import java.util.Map;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -43,7 +37,7 @@ class PostConrollerTest {
     @WithMockUser
     public void shouldReturnDefaultMessageWrongReq() throws Exception {
         this.mockMvc.perform(post("/create")
-                        .param("name","Куплю ладу-грант. Дорого."))
+                        .param("name", "Куплю ладу-грант. Дорого."))
                 .andDo(print())
                 .andExpect(status().is4xxClientError());
     }
@@ -52,7 +46,7 @@ class PostConrollerTest {
     @WithMockUser
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/create")
-                        .param("name","Куплю ладу-грант. Дорого."))
+                        .param("name", "Куплю ладу-грант. Дорого."))
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful());
         ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
@@ -64,7 +58,7 @@ class PostConrollerTest {
     @WithMockUser(username = "test")
     public void testSavePost() throws Exception {
         this.mockMvc.perform(post("/save")
-                        .param("name","Куплю ладу-грант. Дорого.").param("user", "test"))
+                        .param("name", "Куплю ладу-грант. Дорого.").param("user", "test"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection());
         ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
@@ -77,14 +71,10 @@ class PostConrollerTest {
     @Test
     @WithMockUser(username = "test")
     public void testUpdatePost() throws Exception {
-        this.mockMvc.perform(get("/update")
-                        .param("name","Куплю ладу-грант. Дорого."))
-                .andDo(print())
-                .andExpect(status().is2xxSuccessful());
-        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
-        verify(service).saveOrEditPost(argument.capture());
-        assertThat(argument.getValue().getUser().getName(), is("test"));
-        assertThat(argument.getValue().getName(), is("Куплю ладу-грант. Дорого."));
+        Post post = new Post();
+        service.saveOrEditPost(post);
+        Assert.
+
 
     }
 
