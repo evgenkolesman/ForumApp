49a8fa13dcbfa5a9a3ca182e67f15360b915c6f6
evgenkolesman <glavstroi_e@mail.ru>
2021-10-04 11:58:14 +0300

    0. Spring Liquibase [#302330]

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,2 @@
+insert into posts (name, description) values ('О чем этот форум? 1' ,'test');
+insert into posts (name, description) values ('Правила форума. 1', 'test');
--- src/test/java/forum/controller/PostConrollerTest.java
+++ src/test/java/forum/controller/PostConrollerTest.java
@@ -7,14 +7,12 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.ui.Model;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
