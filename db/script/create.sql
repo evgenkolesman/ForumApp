e2b4aadddd64a5139b9c834c0dca136e68d6e670
evgenkolesman <glavstroi_e@mail.ru>
2021-10-05 22:42:15 +0300

    insert change for heroku

--- db/script/create.sql
+++ db/script/create.sql
@@ -1,18 +1,21 @@
-CREATE TABLE IF NOT EXISTS authorities(
-                                          id serial primary key,
-                                          authority text
+CREATE TABLE IF NOT EXISTS authorities
+(
+    id        serial primary key,
+    authority text
 );
-CREATE TABLE IF NOT EXISTS users(
-                                    id serial primary key,
-                                    name text,
-                                    password text,
-                                    authority_id int references authorities(id),
-                                    enabled boolean
+CREATE TABLE IF NOT EXISTS users
+(
+    id           serial primary key,
+    name         text,
+    password     text,
+    authority_id int references authorities (id),
+    enabled      boolean
 );
-CREATE TABLE IF NOT EXISTS posts(
-                                    id serial primary key,
-                                    name text,
-                                    description text,
-                                    created timestamp,
-                                    user_id  int references users(id)
+CREATE TABLE IF NOT EXISTS posts
+(
+    id          serial primary key,
+    name        text,
+    description text,
+    created     timestamp,
+    user_id     int references users (id)
 );
--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,3 +1,10 @@
+INSERT INTO authorities (authority)
+VALUES ('ADMIN');
+insert into authorities (authority)
+values ('ROLE_USER ');
+INSERT INTO users (name, authority_id)
+VALUES ('ADMIN', 1);
+
 
 insert into posts (name, description, user_id) values ('О чем этот форум?' ,'test', 7);
 insert into posts (name, description, user_id) values ('Правила форума.', 'test', 10);
